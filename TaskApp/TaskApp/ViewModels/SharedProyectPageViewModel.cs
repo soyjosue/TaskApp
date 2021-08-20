d032c54bba2425179d30dc87fd55ba45f93be9cf
Elvis Josue Inoa Santiago <josueinoa02@gmail.com>
2021-08-20 16:35:13 -0400

    Se agrego la funcion de que se cambie el codigo de compartir

--- TaskApp/TaskApp/ViewModels/SharedProyectPageViewModel.cs
+++ TaskApp/TaskApp/ViewModels/SharedProyectPageViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Windows.Input;
 using TaskApp.Helper;
+using TaskAppBackend.Models;
 using WebApi.Models;
 using Xamarin.Forms;
 using ZXing.Net.Mobile.Forms;
@@ -16,13 +19,12 @@ namespace TaskApp.ViewModels
         public SharedProyectPageViewModel(Proyect proyect)
         {
             ProyectTitle = proyect.Title;
-            BarcodeValue = JsonConvert.SerializeObject(new
-            {
-                sharedCode = SharedCode,
-                passwordShared = PasswordShared
-            });
+            ProyectId = proyect.Id;
 
             PeopleListPageCommand = new Command(PeopleListPageCommandExecute);
+            ChangeCodeCommand = new Command(ChangeCodeCommandExecute);
+
+            GetCodeShared();
         }
 
         private string barcodeValue;
@@ -37,50 +39,67 @@ namespace TaskApp.ViewModels
             }
         }
 
-
         public string ProyectTitle { get; set; }
 
-        private bool isShared;
-
-        public bool IsShared
-        {
-            get { return isShared; }
-            set
-            {
-                isShared = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private string sharedCode = Guid.NewGuid().ToString();
-
-        public string SharedCode
-        {
-            get { return sharedCode; }
-            set
-            {
-                sharedCode = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public string passwordShared = Guid.NewGuid().ToString();
-
-        public string PasswordShared
-        {
-            get { return passwordShared; }
-            set
-            {
-                passwordShared = value;
-                OnPropertyChanged();
-            }
-        }
-
         public ICommand PeopleListPageCommand { get; set; }
+        public ICommand ChangeCodeCommand { get; set; }
+
+        public int SharedCodeId { get; set; }
+        public int ProyectId { get; set; }
 
         private void PeopleListPageCommandExecute(object obj)
         {
             MessagingCenter.Send(this, Literals.GoToPeopleListPage);
+        }
+
+        private async void ChangeCodeCommandExecute(object obj)
+        {
+            Uri requestUri = new Uri($"{Literals.WEBAPIKEY}/ProyectApi/Shared/{SharedCodeId}");
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add(Literals.TOKEN, Utils.GetToken());
+
+            var json = Utils.ConvertJson(new { });
+
+            try
+            {
+                var response = await client.PutAsync(requestUri, json);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                    GetCodeShared();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private async void GetCodeShared()
+        {
+            Uri requestUri = new Uri($"{Literals.WEBAPIKEY}/ProyectApi/Shared/{ProyectId}");
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add(Literals.TOKEN, Utils.GetToken());
+
+            try
+            {
+                var response = await client.GetAsync(requestUri);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    var sharedProyect = JsonConvert.DeserializeObject<SharedProyect>(await response.Content.ReadAsStringAsync());
+                    BarcodeValue = JsonConvert.SerializeObject(new
+                    {
+                        sharedCode = sharedProyect.Code,
+                        passwordShared = sharedProyect.CodePassword
+                    });
+                    SharedCodeId = sharedProyect.Id;
+                }
+            }
+            catch
+            {
+
+            }
         }
     }
 }
